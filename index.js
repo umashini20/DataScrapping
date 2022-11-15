37e3bc7c5590e5ffc769cc1fd69ff54259dc481f
umashini20 <umashini2000@gmail.com>
2022-11-15 10:16:09 +0530

    multiple pages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
-//const ObjectsToCsv = require('objects-to-csv');
+
 
 
 
@@ -8,10 +8,10 @@ const cheerio = require("cheerio");
 
 const scrapeResults = [];
 let browser;
-let urli = "https://www.linkedin.com/search/results/companies/?companyHqGeo=%5B%22101174742%22%5D&keywords=restaurant&origin=FACETED_SEARCH&sid=S)o&spellCorrectionEnabled=false";
-let pagesToScrape = 2;
-let currentPage = 1;
-let data = [];
+//let urli = "https://www.linkedin.com/search/results/companies/?companyHqGeo=%5B%22101174742%22%5D&keywords=restaurant&origin=FACETED_SEARCH&sid=S)o&spellCorrectionEnabled=false";
+//let pagesToScrape = 2;
+//let currentPage = 1;
+//let data = [];
 
 async function scrapeHeader(){
     try{
@@ -23,54 +23,40 @@ async function scrapeHeader(){
         await page.type("input#password","Uthinksmart7*");
         await page.click("button.btn__primary--large",{delay:20});
         await page.waitForNavigation();
-        await page.goto(
-            urli
-            );
-        while(currentPage<=pagesToScrape){
 
-            const content = await page.content();
-        const $ = await cheerio.load(content);
-        $(".mb1").each((index,element) => {
-            const resultTitle = $(element).find(".app-aware-link");/*.attr("href")*/
-            const restaurantName = resultTitle.text();
-            const url = resultTitle.attr("href");
-            const typeOfIndustry = $(element).find(".entity-result__primary-subtitle");
-            const typeAddress = typeOfIndustry.text();
-            const scrapeResult = {restaurantName, url, typeAddress};
-            scrapeResults.push(scrapeResult);
-            
-        });
-        //console.log($("").text());
-        //console.log(scrapeResults);
-        if(currentPage<pagesToScrape){
-            await page.click(".artdeco-pagination__button",{delay:20});
-            await page.waitForSelector(".artdeco-pagination__button");
+        for(let index = 1; index<=10; index=index+1){
 
-        }
-        currentPage++;
-
-        return scrapeResults;
-
-        
-        
+            await page.goto(
+                //urli
+                "https://www.linkedin.com/search/results/companies/?companyHqGeo=%5B%22101174742%22%5D&keywords=restaurant&origin=FACETED_SEARCH&sid=ayI&spellCorrectionEnabled=false&page=" +index
+                ,{delay:1000});
+            //while(currentPage<=pagesToScrape){
     
+            const content = await page.content();
+            const $ = await cheerio.load(content);
+            $(".entity-result__content").each((index,element) => {
+                const resultTitle = $(element).find(".app-aware-link");/*.attr("href")*/
+                //const restaurantName = resultTitle.text();
+                const url = resultTitle.attr("href");
+                const typeOfIndustry = $(element).find(".entity-result__primary-subtitle");
+                const typeAddress = typeOfIndustry.text();
+                const scrapeResult = {/*restaurantName,*/ url, typeAddress};
+                scrapeResults.push(scrapeResult);
+                //console.log(scrapeResults);
+                
+            });
+            console.log("At page no : "+index);
+           
 
         }
-        
-        
-
-
-
+        return scrapeResults;
+       
     }catch(err){
         console.error(err);
     }
 
 }
-/*async function createCsvFile(data){
-    let csv = new ObjectsToCsv(data);
-    await csv.toDisk('/restaurant.csv');
-   
-}*/
+
 
 
 async function main(){
@@ -83,3 +69,4 @@ async function main(){
 }
 
 main();
+//scrapeHeader();
